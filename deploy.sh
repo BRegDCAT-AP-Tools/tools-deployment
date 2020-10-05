d17ed37ea0acff76786273c4191b84411b90e906
Andres Garcia Mangas <andres.garcia@fundacionctic.org>
2020-10-05 14:30:21 +0200

    Add mapping tool

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,17 @@
 set -e
 set -x
 
-PATH_TOOLS=${PATH_TOOLS:-"./tools"}
+URL_GIT_CREATOR="https://github.com/BRegDCAT-AP-Tools/breg-dcat-creator.git"
+URL_GIT_MAPPING="https://github.com/BRegDCAT-AP-Tools/breg-dcat-mapping.git"
+
+PATH_TOOLS=${PATH_TOOLS:-"tools"}
 PATH_CREATOR=${PATH_CREATOR:-"creator"}
+PATH_MAPPING=${PATH_MAPPING:-"mapping"}
 
 rm -fr ${PATH_TOOLS}
 mkdir -p ${PATH_TOOLS}
 
-git clone https://github.com/BRegDCAT-AP-Tools/breg-dcat-creator.git ${PATH_TOOLS}/${PATH_CREATOR}
+git clone ${URL_GIT_CREATOR} ${PATH_TOOLS}/${PATH_CREATOR}
+git clone ${URL_GIT_MAPPING} ${PATH_TOOLS}/${PATH_MAPPING}
 
 docker-compose up -d --build
